a920eee91c5114f27f1e959d637c6eb9746b0ac9
fcqpl <46460263+fcqpl@users.noreply.github.com>
2021-10-16 18:04:26 +0200

    # to //

--- trunk/src/app/srs_app_rtc_codec.cpp
+++ trunk/src/app/srs_app_rtc_codec.cpp
@@ -236,7 +236,7 @@ srs_error_t SrsAudioTranscoder::init_enc(SrsAudioCodecId dst_codec, int dst_chan
     enc_->sample_rate = dst_samplerate;
     enc_->channels = dst_channels;
     enc_->channel_layout = av_get_default_channel_layout(dst_channels);
-    #enc_->bit_rate = dst_bit_rate;
+    // enc_->bit_rate = dst_bit_rate;
     enc_->bit_rate = 192;
     enc_->sample_fmt = codec->sample_fmts[0];
     enc_->time_base.num = 1; enc_->time_base.den = 1000; // {1, 1000}
